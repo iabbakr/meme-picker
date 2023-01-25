1bb5a8183ea76b72238666d791f560cdf973c7dd
iabbakr <abubakaribrahim603@gmail.com>
2023-01-25 18:16:03 +0100

    added js update

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 import { catsData } from './data.js';
 
 
+const emotionRadios = document.getElementById('emotion-radios')
+
 function getEmotionsArray(cats){
     const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            emotionsArray.push(emotion)
         }
     }
-console.log(emotionsArray)
+    return emotionsArray
 }
+
+
+function renderEmotionsRadios(cats){
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `<p>${emotion}</p>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
