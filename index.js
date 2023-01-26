5a904201f7aa21faf99516e25c9ce937edf566c2
iabbakr <abubakaribrahim603@gmail.com>
2023-01-26 12:48:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            if(emotionsArray.includes(emotion)){
-
-            }else{
-
+            if(emotionsArray.includes =! emotion){
                 emotionsArray.push(emotion)
             }
+
+                
+            
             
 
         }
